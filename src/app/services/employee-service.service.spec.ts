14e37e8106444a82b57392fc68f20485084eb0a7
Kobydon <94402053+Kobydon@users.noreply.github.com>
2022-09-06 14:09:15 +0000

    ready build

--- src/app/services/employee-service.service.spec.ts
+++ src/app/services/employee-service.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { EmployeeServiceService } from './employee-service.service';
+import { EmployeeService } from '../services/employee-service.service';
 
 describe('EmployeeServiceService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: EmployeeServiceService = TestBed.get(EmployeeServiceService);
+    const service: EmployeeService = TestBed.get(EmployeeService);
     expect(service).toBeTruthy();
   });
 });
