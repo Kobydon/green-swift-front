5ea7fecf7edac2204affc244c11d07d6da77827e
Kobydon <94402053+Kobydon@users.noreply.github.com>
2022-09-06 00:34:22 +0000

    h

--- src/server.js
+++ src/server.js
@@ -1,15 +1,11 @@
-//Install express server
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/gng-pi-admin'));
+app.use(express.static('./dist/ng-pi-admin'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/gng-pi-admin/'}),
+    res.sendFile('index.html', { root: 'dist/ng-pi-admin/' }),
 );
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
